f2c82fda32018d65b40461ab36a2c683bdd58e93
rishavcool21 <kumarrishavcool21@gmail.com>
2023-06-30 15:18:39 +0530

    XHRs

---
+++ Section28/MakingXHRs/jsHere.js
@@ -0,0 +1,15 @@
+const req = new XMLHttpRequest();
+
+req.onload = function () {
+    console.log('It Loaded!!');
+    const data = JSON.parse(this.responseText);
+    console.log(data.name, data.height);
+}
+
+req.onerror = function () {
+    console.log('Error!!');
+    console.log(this);
+}
+
+req.open("GET", "https://swapi.dev/api/people/1/");
+req.send();
