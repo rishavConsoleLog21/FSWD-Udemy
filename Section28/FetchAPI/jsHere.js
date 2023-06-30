a501f712e949c575be98e8e721ca156becd0de1f
rishavcool21 <kumarrishavcool21@gmail.com>
2023-06-30 16:09:31 +0530

    fetchAPI

---
+++ Section28/FetchAPI/jsHere.js
@@ -0,0 +1,38 @@
+fetch('https://swapi.dev/api/people/1/')
+    .then((res) => {
+        console.log('Resolved', res);
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        return fetch('https://swapi.dev/api/people/2/');
+    })
+    .then((res) => {
+        console.log('2nd Request Resolved');
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((e) => {
+        console.log('Error', e);
+    });
+
+//using Async function 
+
+// const loadSWPeople = async () => {
+//     try {
+//         const res = await fetch("https://swapi.dev/api/people/1/");
+//         const data = await res.json;
+//         console.log(data);
+//         const res2 = await fetch('https://swapi.dev/api/people/2/');
+//         const data2 = await res2.json;
+//         console.log(data2);
+
+//     }
+//     catch (e) {
+//         console.log("Something went wrong", e);
+//     }
+// };
+
+// loadSWPeople();
