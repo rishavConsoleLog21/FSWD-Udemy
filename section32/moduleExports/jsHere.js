2da7ffe9110b5728c3ea550fbe178cafda006a46
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-03 16:26:39 +0530

    Module Exports

---
+++ section32/moduleExports/jsHere.js
@@ -0,0 +1,9 @@
+const { PI, square, cube } = require('./math');
+
+// const cats = require('./shelter')
+console.log(PI)
+
+console.log(square(9))
+console.log(cube(3))
+// console.log("REQUIRED AN ENTIRE DIRECTORY!", cats)
+
---
+++ section32/moduleExports/math.js
@@ -0,0 +1,17 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = x => x * x;
+
+const cube = x => x * x * x;
+
+exports.square = square;
+exports.PI = PI;
+exports.cube = cube;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+
