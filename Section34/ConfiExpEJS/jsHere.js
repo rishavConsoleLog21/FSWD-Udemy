7423bf0b8c0f58303f53155ac3bf01065e781521
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-05 01:12:13 +0530

    RandomNum in Html

--- Section34/ConfiExpEJS/jsHere.js
+++ Section34/ConfiExpEJS/jsHere.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.render('Home')
  });
 
+app.get('/rand', (req,res) => {
+   const randomNum=Math.floor((Math.random() *21)+1);
+   res.render('random',{rand: randomNum})
+});
+
  app.listen(3000, () => {
     console.log('Listening On Port 3000')
  });
