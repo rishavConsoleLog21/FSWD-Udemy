910328bcc889587e1b90c1c96d00cc1b37f9df33
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-05 23:31:58 +0530

    Complex EJS

--- Section34/ConfiExpEJS/jsHere.js
+++ Section34/ConfiExpEJS/jsHere.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'/views'));
@@ -18,6 +21,17 @@ app.get('/rand/oddeven', (req,res) => {
    const randomNum=Math.floor((Math.random() *21)+1);
    res.render('ConditionalsEJS',{rand: randomNum})
 });
+
+app.get('/r/:subreddit', (req, res) => {
+   const { subreddit } = req.params;
+   const data = redditData[subreddit];
+   if(data) {
+      res.render('subreddit', { ...data });
+   }
+   else{
+      res.render('notfound', { subreddit });
+   }
+})
 
 app.get('/dogs', (req, res) => {
    const dogs = ['Blue', 'Rocky', 'Monty','Stephanie','Woody'
