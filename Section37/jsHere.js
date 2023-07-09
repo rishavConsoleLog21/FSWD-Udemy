93f4c5f603de08eccccf10851d3ef068bfc3acd4
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-10 00:33:13 +0530

    mongo with mongoose

--- Section37/jsHere.js
+++ Section37/jsHere.js
@@ -16,3 +16,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/movieApp', { useNewUrlParser: true})
 //     console.log('we are connected!')
 // });
 
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
