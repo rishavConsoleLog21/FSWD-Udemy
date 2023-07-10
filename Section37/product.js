f1b751456a4197625d3957d442480b8b5fa36d17
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-11 01:13:24 +0530

    Updated Schema Constraints

---
+++ Section37/product.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp', { useNewUrlParser: true})
+    .then(() => {
+        console.log('connection successful') 
+    })
+    .catch((err) => {
+        console.log(`Error connecting to database ${err}`)
+        //console.log(err)
+    });
+
+    const productSchema = new mongoose.Schema({
+        name : {
+            type : String,
+            required : [true,'Name is Required']
+            //unique:[true,"This Name already exists"]
+            //maxlength: 20 (this will limit the length of name)
+            //match(ex- phone no. or email pattern)
+        },
+        price :{
+            type : Number,
+            required : [true, 'Price is Required']
+            //min: and max: (to set the min and max amount of anything)
+        },
+        onSale: {
+            type : Boolean,
+            default: false
+        },
+        categories: {
+            type : [String] //here we are using string inside of array bracket 
+        },
+        qty: {
+            online: {
+                type: Number,
+                default: 0
+            },
+            inStore: {
+                type: Number,
+                default: 0
+            }
+        }
+    });
+
+    const Product = mongoose.model('Product', productSchema);
+
+    const bike = new Product({name: 'Mountain Bike',price: 599, categories: ['Cycling', 'Safety']})
+    bike.save()
+        .then(data => {
+            console.log('It worked')
+            console.log(data);
+        })
+        .catch(err => {
+            console.log('Oh no error')
+            console.log(err)
+        })
