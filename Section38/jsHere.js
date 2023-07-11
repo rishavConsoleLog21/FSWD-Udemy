b5a22fc2b298a05f2ec7be7e22d6cf5232134bd7
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-12 00:55:25 +0530

    Updating Product

--- Section38/jsHere.js
+++ Section38/jsHere.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
+
 
 const Product =require('./models/product');
 
@@ -20,6 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
@@ -49,6 +52,12 @@ app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true})
+    res.redirect(`/products/${product._id}`)
 })
 
 app.listen(3000, () => {
