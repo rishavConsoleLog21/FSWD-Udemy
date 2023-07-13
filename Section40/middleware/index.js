dc2a12bfb2719feb5837f4a4ffd905b5fa257a27
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-14 00:30:29 +0530

    Setup 404 Route

--- Section40/middleware/index.js
+++ Section40/middleware/index.js
@@ -4,12 +4,29 @@ const morgan = require('morgan');
 
 app.use(morgan('tiny'))
 
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
+
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!!")
+    next();
+})
+
+
 app.get('/', (req, res)=> {
     res.send('Home page')
 })
 
 app.get('/dogs', (req, res)=> {
     res.send('WOOOFF')
+})
+
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
 })
 
 app.listen(3000, () => {
