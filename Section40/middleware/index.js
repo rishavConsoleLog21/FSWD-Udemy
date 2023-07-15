9c03d7ba43a75f08cccc421b1f036be18d6952a0
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-15 16:14:39 +0530

    Added Custom Error

--- Section40/middleware/index.js
+++ Section40/middleware/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const AppError = require('./appError');
+
 app.use(morgan('tiny'))
 
 app.use((req, res, next) => {
@@ -20,8 +22,9 @@ const verifyPassword = (req, res, next) => {
     if (password === 'chickennugget') {
         next();
     }
+    throw new AppError('Password required',401);
     // res.send("YOU NEED A PASSWORD!")
-    throw new Error('Password Required')
+    // throw new Error('Password Required')
 }
 
 app.get('/error', (req, res) => {
@@ -40,14 +43,25 @@ app.get('/secret', verifyPassword, (req, res) => {
     res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
 })
 
+app.get('/admin', (req, res) => {
+    throw new AppError('You Are Not An Admin', 403)
+})
+
 app.use((req, res) => {
     res.status(404).send('NOT FOUND!')
 })
 
+// app.use((err, req, res, next) => {
+//     console.log('******************************')
+//     console.log('************ERROR*************')
+//     console.log('******************************')
+//     console.log(err)
+//     next(err)
+// })
+
 app.use((err, req, res, next) => {
-    console.log('******************************')
-    console.log('************ERROR*************')
-    console.log('******************************')
+    const { status = 500, message = 'Something Went Wrong'} = err;
+    res.status(status).send(message)
 })
 
 app.listen(3000, () => {
