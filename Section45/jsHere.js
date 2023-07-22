27fd6973355002bcdbebfc6099efa39bbadc42ae
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-22 18:37:05 +0530

    Creating product for a farm

--- Section45/jsHere.js
+++ Section45/jsHere.js
@@ -47,8 +47,22 @@ app.get('/farms/:id', async (req, res) => {
     res.render('farms/show.ejs', {farm})
 })
 
+app.get('/farms/:id/products/newProduct', (req,res) => {
+    const {id} = req.params;
+    res.render('products/newProduct', {categories, id})
+})
 
-
+app.post('/farms/:id/products', async (req, res) => {
+    const {id} = req.params;
+    const farm = await Farm.findById(id);
+    const {name, price, category} = req.body;
+    const product = new Product({name, price, category});
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.send(farm);
+})
 
 
 // PRODUCT ROUTES   
