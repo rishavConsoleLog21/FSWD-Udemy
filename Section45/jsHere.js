ad8cb43f71a7f3abf92835c96835f1460dc7a810
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-22 17:54:59 +0530

    Added Show Page

--- Section45/jsHere.js
+++ Section45/jsHere.js
@@ -42,6 +42,12 @@ app.post('/farms', async(req, res) => {
     res.redirect('/farms')
 })
 
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id);
+    res.render('farms/show.ejs', {farm})
+})
+
+
 
 
 
