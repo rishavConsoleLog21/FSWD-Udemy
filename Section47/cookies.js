b6516082004b7cb230e28e7490f88fd0ab75aab4
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-24 00:08:50 +0530

    Added Signing Cookies

--- Section47/cookies.js
+++ Section47/cookies.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('secret'));
 
 app.get('/greet', (req, res) => {
     const { name = 'anonyms'} = req.cookies;
@@ -13,6 +13,17 @@ app.get('/setname', (req,res)=> {
     res.cookie('name', 'steve');
     res.cookie('animal', 'harlequin shrimp')
     res.send('ok sent u a cookie')
+})
+
+app.get('/getsignedcookie', (req,res,) => {
+    res.cookie('fruit', 'grape', {signed: true})
+    res.send('ok signed ur cookie')
+})
+
+app.get('/verifyfruit', (req,res,) => {
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, (req,res) => {
