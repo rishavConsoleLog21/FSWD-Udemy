78bc2dbf1017017d21850a8826ca3605425c4a3a
rishavcool21 <kumarrishavcool21@gmail.com>
2023-07-25 00:04:58 +0530

    Added more express session

--- Section48/index.js
+++ Section48/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const session  = require('express-session');
 
-app.use(session({secret: 'thisissecret'}));
+const sessionOption = {secret: 'thisissecret', resave: false, saveUninitialized: false}
+app.use(session(sessionOption));
 
 app.get('/viewcount', (req,res) => {
     if(req.session.count){
@@ -11,6 +12,16 @@ app.get('/viewcount', (req,res) => {
         req.session.count = 1;
     }
     res.send(`You have viewed this page ${req.session.count} times`)
+})
+
+app.get('/register', (req, res) => {
+    const {username = 'Anonymous'} = req.query;
+    req.session.username = username;
+})
+
+app.get('/greet', (req,res) => {
+    const {username} = req.session;
+    res.send(`Welcoome back, ${username}`)
 })
 
 app.listen(3000, (req,res) => {
