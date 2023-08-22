c2a14173c3bfcd654284dff34ff94fab99acb575
rishavcool21 <kumarrishavcool21@gmail.com>
2023-08-22 23:25:54 +0530

    Added React Prop

--- Section62/first-react-app/src/App.jsx
+++ Section62/first-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import RandomPokemon from "./RandomPokemon"
+import Greeter from './Greeter'
 import './App.css'
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
     <>
       <h1>Guess The Pokemon ?</h1>
       <div>
-        <RandomPokemon/>
+        <Greeter person="Rishav" />
+        {/* <RandomPokemon/> */}
       </div>
     </>
   )
---
+++ Section62/first-react-app/src/Greeter.jsx
@@ -0,0 +1,3 @@
+export default function Greeter({person}) {
+    return <h1>Hi There!!!{person}</h1>
+}
