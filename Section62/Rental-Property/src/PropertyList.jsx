56f77cd12a2555afec91fad78e96e8ca35117030
rishavcool21 <kumarrishavcool21@gmail.com>
2023-08-24 23:51:41 +0530

    Added Rental Property

---
+++ Section62/Rental-Property/src/PropertyList.jsx
@@ -0,0 +1,12 @@
+import Property from "./Property"
+import "./PropertyList.css"
+export default function PropertyList({properties}) {
+    return (
+        <div className="PropertyList">
+            {properties.map(p => {
+                return <Property {...p} key={p.id}/> //using spread operator
+                    // <Property name={p.name} price={p.price} rating={p.rating}/>
+            })}
+        </div>
+    )
+}
