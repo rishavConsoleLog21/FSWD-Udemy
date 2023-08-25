f27ae54b5b2170377502f99efcdefafc7dd0c5fb
rishavcool21 <kumarrishavcool21@gmail.com>
2023-08-25 23:48:41 +0530

    Added Clicker useState

---
+++ Section66/React-State/Hook-State/src/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Counter() {
+    const [num, setNum] = useState(0);
+    
+    const changeNum = () => {
+        setNum(num+1);
+    }
+    return (
+        <div>
+            <p>The count is: {num}</p>
+            <button onClick={changeNum}>Increment</button>
+        </div>
+    )
+}
