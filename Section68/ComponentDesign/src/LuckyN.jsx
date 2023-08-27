2eb29b23cd181c4a405de00c0ca37e4fd295045c
rishavcool21 <kumarrishavcool21@gmail.com>
2023-08-27 19:48:34 +0530

    Passing Fnc that update state

--- Section68/ComponentDesign/src/LuckyN.jsx
+++ Section68/ComponentDesign/src/LuckyN.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import {getRolls } from "./utils"
 import Dice from "./Dice";
+import Button from "./Button";
+
 function LuckyN({title="Dice Game", numDice=2, winCheck}) {
     const [dice, setDice] = useState(getRolls(numDice))
     const isWinner = winCheck(dice);
@@ -10,7 +12,8 @@ function LuckyN({title="Dice Game", numDice=2, winCheck}) {
             <h1>{title}{winCheck} {isWinner ? "🥳You Win!!🥳" : "Keep Trying!!"}</h1>
             {isWinner && <h2>Congrats!</h2>}
             <Dice dice={dice}/>
-            <button onClick={roll}>Re-Rolls Dice</button>
+            {/* <button onClick={roll}>Re-Rolls Dice</button> */}
+            <Button clickFunc={roll} label="Re-Roll"/>
         </main>
     )
 }
